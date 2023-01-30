86960a0b28dc5dc99a01fe3ee3873f157b9f5b33
Romina da graca <romidagraca@gmail.com>
2023-01-30 19:22:39 +0100

    empezando a trabajar con node

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const path =require('path');
+var mysql=require("mysql");
+var bodyparser=require("body-parser");
+const app = express()
+const port = 3000
+
+app.use(bodyparser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+let options={
+    root:path.join(__dirname)
+}
+  res.sendFile("/views/index.html",options);
+})
+
+
+app.post("/",(req,res)=>{
+    var conn=mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'',
+        database:'biblioteca'
+    });
+    conn.connect();
+    var libro={
+        'codigo':req.body.codigo,
+        'titulo':req.body.titulo,
+        'genero':req.body.genero
+    }
+    var sql=`insert into libros set ?`;
+    conn.query(sql,libro,(err,result,field)=>{
+        if(err){
+            console.log(err);
+            res.send("Error conectando a la bbdd");
+        }
+        console.log(result)
+        res.redirect("/");
+        conn.end();
+    })
+     
+
+    
+})
+
+
+app.get("/libros",(req,res)=>{
+
+    res.send("hola que tal");
+
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
